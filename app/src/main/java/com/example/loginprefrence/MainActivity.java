2adfff7acc666bef90c9c4b2aed1796f2fa59bc8
sandeep <kumarjha.sandeep@gmail.com>
2019-04-19 16:11:16 +0530

    once log in and keep loggedin until you erase the storage.

---
+++ app/src/main/java/com/example/loginprefrence/MainActivity.java
@@ -0,0 +1,135 @@
+package com.example.loginprefrence;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.content.SharedPreferences;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText mEmail;
+    private EditText mPassWord;
+    private CheckBox rememberMe;
+    private Button signinbtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //Here we will validate saved preferences
+
+            Log.d("Thesandx", "inside oncreate");
+
+            setContentView(R.layout.activity_main);
+
+            if(isLoggedin()){
+                startHomeActivity();
+            }
+
+            mEmail = findViewById(R.id.email);
+
+            mPassWord = findViewById(R.id.password);
+            signinbtn = findViewById(R.id.sign_in_button);
+            rememberMe = findViewById(R.id.rememberMe);
+
+            signinbtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    loginAttempt();
+
+                }
+            });
+
+        }
+
+
+    private void loginAttempt() {
+        //to show error,now set it to null by default
+        mEmail.setError(null);
+        mPassWord.setError(null);
+
+
+        //get value from edittext
+        String email = mEmail.getText().toString();
+        String password = mPassWord.getText().toString();
+
+        boolean cancel = false;
+
+        View focusView = null;
+
+        //check for valid password
+
+        if (TextUtils.isEmpty(password)) {
+            mPassWord.setError("can't left blank");
+
+            focusView = mPassWord;
+            cancel = true;
+
+        }
+
+        //check for valid email id
+
+        if (TextUtils.isEmpty(email)) {
+            mEmail.setError("it can't be left empty");
+            focusView = mEmail;
+            cancel = true;
+        } else if (!isEmailValid(email)) {
+
+            mEmail.setError("wrong email address");
+            focusView = mEmail;
+            cancel = true;
+        }
+
+
+        if (cancel) {
+            focusView.requestFocus();
+        } else {
+            //save data in local shared prefrences
+           if (rememberMe.isChecked()) {
+                saveLoginDetails(email, password,true);
+            }
+
+            startHomeActivity();
+          //  Toast.makeText(MainActivity.this, "Signing in...", Toast.LENGTH_SHORT).show();
+
+        }
+
+    }
+
+    private void startHomeActivity () {
+
+        Intent intent = new Intent(this, HomeActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    private void saveLoginDetails(String email, String password,Boolean logged) {
+        new PrefManager(this).saveLoginDetails(email, password);
+
+    }
+
+    private boolean isLoggedin() {
+        Log.d("Thesandx","inside isloggedin");
+
+      return new PrefManager(this).isLoggedIn();
+
+     // Toast.makeText(MainActivity.this,email,Toast.LENGTH_SHORT).show();
+    }
+
+    private boolean isEmailValid(String email) {
+
+        return email.contains("@");
+    }
+
+    private boolean isPasswordValid(String password) {
+
+        return password.length() > 4;
+    }
+
+}
---
+++ app/src/main/java/com/example/loginprefrence/PrefManager.java
@@ -0,0 +1,38 @@
+package com.example.loginprefrence;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class PrefManager {
+
+    Context context;
+
+    PrefManager(Context context){
+        this.context = context;
+    }
+
+    public void saveLoginDetails(String email,String password){
+        SharedPreferences sharedPreferences = context.getSharedPreferences("LoginDetails",Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("Email",email);
+        editor.putString("PassWord",password);
+        editor.putBoolean("Logged",true);
+        editor.apply();
+
+    }
+
+    public boolean isLoggedIn() {
+        SharedPreferences sharedPreferences = context.getSharedPreferences("LoginDetails", Context.MODE_PRIVATE);
+        return sharedPreferences.getBoolean("Logged",false);
+
+    }
+
+    public boolean isUserLogedOut() {
+        SharedPreferences sharedPreferences = context.getSharedPreferences("LoginDetails", Context.MODE_PRIVATE);
+        boolean isEmailEmpty = sharedPreferences.getString("Email", "").isEmpty();
+        boolean isPasswordEmpty = sharedPreferences.getString("Password", "").isEmpty();
+        return isEmailEmpty;
+    }
+
+
+}
